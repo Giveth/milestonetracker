0574b06e1f4a05889b9d7bdd2b904894aa0f077b
Jordi Baylina <jordi@baylina.cat>
2016-11-27 12:34:56 +0100

    License and version dependencies

--- MilestoneTracker.sol
+++ MilestoneTracker.sol
@@ -1,3 +1,26 @@
+/*
+    Copyright 2016, Jordi Baylina
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/// @title MilestoneTracker Contract
+/// @author Jordi Baylina
+/// @dev This contract is rules the relation betwen a donor and a recipient
+///  in order to guaranty to the donor that the job will be done and to guaranty
+///  to the recipient that he will be paid
+
 pragma solidity ^0.4.4;
 
 // We use the RLP library to decode RLP
